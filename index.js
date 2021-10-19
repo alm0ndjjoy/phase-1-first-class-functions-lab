95a94c703e595fccddbda017155af0a80aa5c407
Joyce Huang <65410999+alm0ndjjoy@users.noreply.github.com>
2021-10-19 10:08:31 -0400

    commit tree d7b0962ff6812a7fc8f7deda07d9d1f3cfa3b4e9

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+fareTripler(15)
+
+function returnFirstTwoDrivers(drivers){
+    return [drivers[0], drivers[1]]
+}
+
+function returnLastTwoDrivers(drivers){
+    return drivers.slice (-2)
+}
+
+function createFareMultiplier (initial){
+    return function(secondNumber){
+        return initial * secondNumber;
+    }
+}
+
+
+function selectDifferentDrivers(drivers=['Sally', 'Bob', 'Freddy', 'Claudia'], a ){
+    
+
+    return a(drivers);
+}
